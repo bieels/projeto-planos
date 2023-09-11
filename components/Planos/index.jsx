12fbcc1990ed52ed3ee71897e3ffe30bd887ca50
Gabriel Santos <bieelss01@gmail.com>
2023-09-11 06:04:04 -0300

    update

--- components/Planos/index.jsx
+++ components/Planos/index.jsx
@@ -3,104 +3,10 @@ import "slick-carousel/slick/slick-theme.css";
 import "./styles.css";
 import { plans } from "../../Data.json";
 import Slider from "react-slick";
+import { settings } from "../../utils/carousel"
 
 export default function Planos() {
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    adaptiveWidth: true,
-    responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 3.5,
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint: 1260,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          initialSlide: 3
-        }
-      },
-      {
-        breakpoint: 1080,
-        settings: {
-          slidesToShow: 2.7,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 1010,
-        settings: {
-          slidesToShow: 2.5,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 990,
-        settings: {
-          slidesToShow: 2.3,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 810,
-        settings: {
-          slidesToShow: 1.9,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 770,
-        settings: {
-          slidesToShow: 1.8,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 730,
-        settings: {
-          slidesToShow: 1.6,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
+  
 
   return (
     <>
---
+++ utils/carousel.js
@@ -0,0 +1,95 @@
+export const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3.5,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 1260,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          initialSlide: 3
+        }
+      },
+      {
+        breakpoint: 1080,
+        settings: {
+          slidesToShow: 2.7,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 1010,
+        settings: {
+          slidesToShow: 2.5,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 2.3,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 810,
+        settings: {
+          slidesToShow: 1.9,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 1.8,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 730,
+        settings: {
+          slidesToShow: 1.6,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
