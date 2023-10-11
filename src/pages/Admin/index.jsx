4dc126abac76fa7c0cad83b3e5c549433762a71c
Gabriel Santos <bieelss01@gmail.com>
2023-10-11 19:19:39 -0300

    Update

---
+++ src/pages/Admin/index.jsx
@@ -0,0 +1,377 @@
+import React, { useState, useEffect } from "react";
+import { settings } from "../../utils/carousel";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import axios from "axios";
+import "./styles.css";
+
+export default function CrudPage() {
+  const [items, setItems] = useState([]);
+  const [idToEdit, setIdToEdit] = useState("");
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [desc1, setDesc1] = useState("");
+  const [desc2, setDesc2] = useState("");
+  const [desc3, setDesc3] = useState("");
+  const [desc4, setDesc4] = useState("");
+  const [appBasico, setAppBasico] = useState(false);
+  const [desc5, setDesc5] = useState("");
+  const [appPremium, setAppPremium] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://json-server-dw.vercel.app/plans"
+        );
+        setItems(response.data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const handleCreate = async () => {
+    try {
+      const newItem = {
+        title,
+        price,
+        desc1,
+        desc2,
+        desc3,
+        desc4,
+        appbasico: appBasico,
+        desc5,
+        apppremium: appPremium,
+      };
+      const response = await axios.post(
+        "https://json-server-dw.vercel.app/plans",
+        newItem
+      );
+      newItem.id = response.data.id;
+      setItems([...items, newItem]);
+      clearForm();
+    } catch (error) {
+      console.error("Error creating item: ", error);
+    }
+  };
+
+  const handleEdit = (item) => {
+    setIdToEdit(item.id);
+    setTitle(item.title);
+    setPrice(item.price);
+    setDesc1(item.desc1);
+    setDesc2(item.desc2);
+    setDesc3(item.desc3);
+    setDesc4(item.desc4);
+    setAppBasico(item.appbasico);
+    setDesc5(item.desc5);
+    setAppPremium(item.apppremium);
+    setShowEditModal(true);
+  };
+
+  const handleSaveEdit = async () => {
+    try {
+      const updatedItem = {
+        title,
+        price,
+        desc1,
+        desc2,
+        desc3,
+        desc4,
+        appbasico: appBasico,
+        desc5,
+        apppremium: appPremium,
+      };
+      await axios.put(
+        `https://json-server-dw.vercel.app/plans/${idToEdit}`,
+        updatedItem
+      );
+
+      const updatedItems = items.map((item) =>
+        item.id === idToEdit ? { ...item, ...updatedItem } : item
+      );
+
+      setItems(updatedItems);
+      clearForm();
+    } catch (error) {
+      setShowEditModal(false);
+      console.error("Error editing item: ", error);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`https://json-server-dw.vercel.app/plans/${idToEdit}`);
+      const updatedItems = items.filter((item) => item.id !== idToEdit);
+      setItems(updatedItems);
+      clearForm();
+    } catch (error) {
+      setShowEditModal(false);
+      console.error("Error deleting item: ", error);
+    }
+  };
+
+  const clearForm = () => {
+    setIdToEdit("");
+    setTitle("");
+    setPrice("");
+    setDesc1("");
+    setDesc2("");
+    setDesc3("");
+    setDesc4("");
+    setAppBasico(false);
+    setDesc5("");
+    setAppPremium(false);
+  };
+
+  return (
+    <section className="crud-page">
+      <main id="planos" className="planos">
+        <h2 className="planos-titulo">Editar Planos</h2>
+        <Slider className="slider-container" {...settings}>
+          {items.map((item) => (
+            <div key={item.id} className="cards">
+              <article className="data-card">
+                <h3>{item.title}</h3>
+                <h4>{item.price}</h4>
+                <div className="plan-desc">
+                  {" "}
+                  <p>{item.desc1}</p> <br />
+                  <p>{item.desc2}</p> <br />
+                  <p>{item.desc3}</p> <br />
+                  <p>{item.desc4}</p> <br />
+                  <p>{item.desc5}</p>
+                </div>
+                <a
+                  href="#planos"
+                  className="link-text"
+                  onClick={() => handleEdit(item)}
+                >
+                  Editar
+                  <svg
+                    width="25"
+                    height="16"
+                    viewBox="0 0 25 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M17.8631 0.929124L24.2271 7.29308C24.6176 7.68361 24.6176 8.31677 24.2271 8.7073L17.8631 15.0713C17.4726 15.4618 16.8394 15.4618 16.4489 15.0713C16.0584 14.6807 16.0584 14.0476 16.4489 13.657L21.1058 9.00019H0.47998V7.00019H21.1058L16.4489 2.34334C16.0584 1.95281 16.0584 1.31965 16.4489 0.929124C16.8394 0.538599 17.4726 0.538599 17.8631 0.929124Z"
+                      fill="var(--fonte-preta)"
+                    />
+                  </svg>
+                </a>
+              </article>
+            </div>
+          ))}
+        </Slider>
+      </main>
+      <div className="crud-container">
+
+        {showEditModal && (
+          <div className="edit-modal">
+            <div className="edit-modal-content">
+              <h2>Editar Item</h2>
+              <div className="input-container">
+                <label htmlFor="title">Título:</label>
+                <input
+                  type="text"
+                  id="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <span className="bar"></span>
+              </div>
+              <div className="input-container">
+                <label htmlFor="price">Preço:</label>
+                <input
+                  type="text"
+                  id="price"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+                <span className="bar"></span>
+              </div>
+              <div className="input-container">
+                <label htmlFor="desc1">Descrição 1:</label>
+                <input
+                  type="text"
+                  id="desc1"
+                  value={desc1}
+                  onChange={(e) => setDesc1(e.target.value)}
+                />
+                <span className="bar"></span>
+              </div>
+              <div className="input-container">
+                <label htmlFor="desc2">Descrição 2:</label>
+                <input
+                  type="text"
+                  id="desc2"
+                  value={desc2}
+                  onChange={(e) => setDesc2(e.target.value)}
+                />
+                <span className="bar"></span>
+              </div>
+              <div className="input-container">
+                <label htmlFor="desc3">Descrição 3:</label>
+                <input
+                  type="text"
+                  id="desc3"
+                  value={desc3}
+                  onChange={(e) => setDesc3(e.target.value)}
+                />
+                <span className="bar"></span>
+              </div>
+              <div className="input-container">
+                <label htmlFor="desc4">Descrição 4:</label>
+                <input
+                  type="text"
+                  id="desc4"
+                  value={desc4}
+                  onChange={(e) => setDesc4(e.target.value)}
+                />
+                <span className="bar"></span>
+              </div>
+              <div className="input-container">
+                <label htmlFor="appBasico">App Básico:</label>
+                <input
+                  type="checkbox"
+                  id="appBasico"
+                  checked={appBasico}
+                  onChange={(e) => setAppBasico(e.target.checked)}
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="desc5">Descrição 5:</label>
+                <input
+                  type="text"
+                  id="desc5"
+                  value={desc5}
+                  onChange={(e) => setDesc5(e.target.value)}
+                />
+                <span className="bar"></span>
+              </div>
+              <div className="input-container">
+                <label htmlFor="appPremium">App Premium:</label>
+                <input
+                  type="checkbox"
+                  id="appPremium"
+                  checked={appPremium}
+                  onChange={(e) => setAppPremium(e.target.checked)}
+                />
+              </div>
+             <div className="buttons">
+             <button onClick={handleSaveEdit}>Salvar</button>
+              <button onClick={handleDelete}>Excluir</button>
+              <button onClick={() => setShowEditModal(false)}>Fechar</button>
+             </div>
+            </div>
+          </div>
+        )}
+
+        <div className="form-container">
+          <h2>Criar Plano</h2>
+          <div className="input-container">
+            <label htmlFor="title">Título:</label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <span className="bar"></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="price">Preço:</label>
+            <input
+              type="text"
+              id="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            <span className="bar"></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="desc1">Descrição 1:</label>
+            <input
+              type="text"
+              id="desc1"
+              value={desc1}
+              onChange={(e) => setDesc1(e.target.value)}
+            />
+            <span className="bar"></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="desc2">Descrição 2:</label>
+            <input
+              type="text"
+              id="desc2"
+              value={desc2}
+              onChange={(e) => setDesc2(e.target.value)}
+            />
+            <span className="bar"></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="desc3">Descrição 3:</label>
+            <input
+              type="text"
+              id="desc3"
+              value={desc3}
+              onChange={(e) => setDesc3(e.target.value)}
+            />
+            <span className="bar"></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="desc4">Descrição 4:</label>
+            <input
+              type="text"
+              id="desc4"
+              value={desc4}
+              onChange={(e) => setDesc4(e.target.value)}
+            />
+            <span className="bar"></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="appBasico">App Básico:</label>
+            <input
+              type="checkbox"
+              id="appBasico"
+              checked={appBasico}
+              onChange={(e) => setAppBasico(e.target.checked)}
+            />
+            <span className="bar"></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="desc5">Descrição 5:</label>
+            <input
+              type="text"
+              id="desc5"
+              value={desc5}
+              onChange={(e) => setDesc5(e.target.value)}
+            />
+            <span className="bar"></span>
+          </div>
+          <div className="input-container">
+            <label htmlFor="appPremium">App Premium:</label>
+            <input
+              type="checkbox"
+              id="appPremium"
+              checked={appPremium}
+              onChange={(e) => setAppPremium(e.target.checked)}
+            />
+            <span className="bar"></span>
+          </div>
+          <button onClick={handleCreate}>Criar</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,29 @@
+import NavBar from "../../components/NavBar";
+import Destaques from "../../components/Destaques";
+import Inicio from "../../components/Inicio";
+import Planos from "../../components/Planos";
+import Sobre from "../../components/Sobre";
+import Diferenciais from "../../components/Diferenciais";
+import Contato from "../../components/Contato";
+import Footer from "../../components/Footer";
+import Modal from "../../components/Modal";
+import PlanoPopup from "../../components/PlanoPopup";
+
+import { AppProvider } from "../../context/AppContext";
+
+export default function Home() {
+  return (
+    <>
+      <AppProvider>
+        <Modal children={<PlanoPopup />}/>
+        <Inicio />
+        <Destaques />
+        <Sobre />
+        {/* <Diferenciais /> */}
+        <Planos />
+        <Contato />
+      </AppProvider>
+    </>
+  );
+}
+
